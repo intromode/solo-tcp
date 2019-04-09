f5a1fe406defa08f12f6907e845c54a5d4dd6794
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-09 09:53:23 -0700

    reading from file writing to file working

---
+++ readWrite.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const rs = fs.createReadStream('./2_tcp.md', {
+  encoding: 'utf8'
+});
+
+const ws = fs.createWriteStream('./2_tcp-copy.md');
+
+
+rs.on('data', data => {
+  ws.write(data);
+})
